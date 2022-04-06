b7c87b69fad3a8aad1767e140030966ae6d3e53d
ssuedha <100778116+ssuedha@users.noreply.github.com>
2022-04-06 15:43:02 +0200

    Delete ceo-repository.ts

--- src/app/domain/ceo/ceo-repository.ts
+++
@@ -1,9 +0,0 @@
-import { CEO } from './model/ceo';
-
-export abstract class CEORepository {
-    abstract getCEOById(id: string): Promise<CEO>;
-
-    abstract getAllCEOs(): Promise<CEO[]>;
-
-    abstract updateCEO(): Promise<CEO>;
-}
