8fd0befeef07540ed2b8f91cb8b1b5b4ec86e124
Weronika Piotrowska <w.piotrowska00@wp.pl>
2022-04-27 10:06:20 +0200

    Fix code formatting

--- src/app/presentation/pages/client/client.component.ts
+++ src/app/presentation/pages/client/client.component.ts
@@ -46,7 +46,6 @@ export class ClientComponent implements OnInit {
   loremIpsum = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!';
 
   constructor() {
-
   }
 
   ngOnInit(): void {
@@ -55,34 +54,26 @@ export class ClientComponent implements OnInit {
 
   initializeSampleData(): void {
     //TEMPORARY 
-    this.deposits = [
-      {
-        accountId: '1',
-        moneyAmount: 200,
-        interestRate: 10,
-        startDate: new Date(1999, 10, 15, 0, 0, 0),
-        endDate: new Date(2023, 10, 15, 0, 0, 0)
-
-      },
-      {
-        accountId: '2',
-        moneyAmount: 3000,
-        interestRate: 500,
-        startDate: new Date(2000, 1, 4, 0, 0, 0),
-        endDate: new Date(2030, 1, 4, 0, 0, 0)
-
-      }
-
-    ]
-    this.loans = [
-      {
-        accountId: '2',
-        amountToPay: 5000,
-        interestRate: 300,
-        startDate: new Date(2001, 1, 4, 0, 0, 0),
-        endDate: new Date(2023, 1, 4, 0, 0, 0)
-      }
-    ]
-
+    this.deposits = [{
+      accountId: '1',
+      moneyAmount: 200,
+      interestRate: 10,
+      startDate: new Date(1999, 10, 15, 0, 0, 0),
+      endDate: new Date(2023, 10, 15, 0, 0, 0)
+    },
+    {
+      accountId: '2',
+      moneyAmount: 3000,
+      interestRate: 500,
+      startDate: new Date(2000, 1, 4, 0, 0, 0),
+      endDate: new Date(2030, 1, 4, 0, 0, 0)
+    }]
+    this.loans = [{
+      accountId: '2',
+      amountToPay: 5000,
+      interestRate: 300,
+      startDate: new Date(2001, 1, 4, 0, 0, 0),
+      endDate: new Date(2023, 1, 4, 0, 0, 0)
+    }]
   }
 }
