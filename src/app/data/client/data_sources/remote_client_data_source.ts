3c29cc1334d1058017ab2193826865e7bb8511eb
przemekkapica <przemekapica@gmail.com>
2022-05-17 22:03:07 +0200

    Add network configuration and client data source implementation

--- src/app/data/client/data_sources/remote_client_data_source.ts
+++ src/app/data/client/data_sources/remote_client_data_source.ts
@@ -1,26 +1,72 @@
-import { AccountDTO } from "../../account/dtos/account-dto";
+import { HttpClient } from "@angular/common/http";
+import { lastValueFrom } from "rxjs";
+import { NetworkConfiguration } from "src/app/core/config/network_configuration";
+import { DepositDTO } from "../../deposit/dtos/deposit-dto";
+import { LoanDTO } from "../../loan/dtos/loan-dto";
 import { ClientDTO } from "../dtos/client-dto";
 import { ClientDataSource } from "./client_data_source";
 
 export class RemoteClientDataSource implements ClientDataSource {
-    getClientById(id: string): Promise<ClientDTO> {
-        throw new Error("Method not implemented.");
+
+    constructor(private http: HttpClient) {}    
+
+    getClient(clientId: string): Promise<ClientDTO> {
+        return lastValueFrom(this.http.get<ClientDTO>(NetworkConfiguration.getClient(clientId)));
     }
 
-    getAllClients(): Promise<ClientDTO[]> {
-        throw new Error("Method not implemented.");
+    getClients(): Promise<ClientDTO[]> {
+        return lastValueFrom(this.http.get<ClientDTO[]>(NetworkConfiguration.getClients()));
     }
 
-    updateClient(): Promise<ClientDTO> {
-        throw new Error("Method not implemented.");
+    getLoans(clientId: string): Promise<LoanDTO[]> {
+        return lastValueFrom(this.http.get<LoanDTO[]>(NetworkConfiguration.getLoans(clientId)));
     }
 
-    payInstallment(id: string): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    
-    createAccount(account: AccountDTO): Promise<void> {
-        throw new Error("Method not implemented.");
+    getDeposits(clientId: string): Promise<DepositDTO[]> {
+        return lastValueFrom(this.http.get<DepositDTO[]>(NetworkConfiguration.getDeposits(clientId)));
     }
 
+    getLoan(clientId: string, loanId: string): Promise<LoanDTO> {
+        return lastValueFrom(this.http.get<LoanDTO>(NetworkConfiguration.getLoan(clientId, loanId)));
+    }
+
+    getDeposit(clientId: string, depositId: string): Promise<DepositDTO> {
+        return lastValueFrom(this.http.get<DepositDTO>(NetworkConfiguration.getDeposit(clientId, depositId)));
+    }
+
+    createClient(client: ClientDTO): Promise<void> {
+        return lastValueFrom(this.http.post<void>(NetworkConfiguration.createClient(), client));
+    }
+
+    createLoan(clientId: string, loan: LoanDTO): Promise<void> {
+        return lastValueFrom(this.http.post<void>(NetworkConfiguration.createLoan(clientId), loan));
+    }
+
+    createDeposit(clientId: string, deposit: DepositDTO): Promise<void> {
+        return lastValueFrom(this.http.post<void>(NetworkConfiguration.createDeposit(clientId), deposit));
+    }
+
+    updateClient(client: ClientDTO): Promise<ClientDTO> {
+        return lastValueFrom(this.http.put<ClientDTO>(NetworkConfiguration.updateClient(), client));
+    }
+
+    updateLoan(clientId: string, loan: LoanDTO): Promise<LoanDTO> {
+        return lastValueFrom(this.http.put<LoanDTO>(NetworkConfiguration.updateLoan(clientId), loan));
+    }
+
+    updateDeposit(clientId: string, deposit: DepositDTO): Promise<DepositDTO> {
+        return lastValueFrom(this.http.put<DepositDTO>(NetworkConfiguration.updateDeposit(clientId), deposit));
+    }
+
+    deleteClient(clientId: string): Promise<void> {
+        return lastValueFrom(this.http.delete<void>(NetworkConfiguration.deleteClient(clientId)));
+    }
+
+    deleteLoan(clientId: string, loanId: string): Promise<void> {
+        return lastValueFrom(this.http.delete<void>(NetworkConfiguration.deleteLoan(clientId, loanId)));
+    }
+
+    deleteDeposit(clientId: string, depositId: string): Promise<void> {
+        return lastValueFrom(this.http.delete<void>(NetworkConfiguration.deleteDeposit(clientId, depositId)));
+    }
 }
