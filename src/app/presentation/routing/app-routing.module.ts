826c69983707eb22c5e3afcc699a0e581c2dd7b4
Weronika Piotrowska <w.piotrowska00@wp.pl>
2022-05-18 12:12:44 +0200

    Resolve merge conflict

--- src/app/presentation/routing/app-routing.module.ts
+++ src/app/presentation/routing/app-routing.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminPanelComponent } from '../pages/admin-panel/admin-panel.component';
-import { ClientComponent } from '../pages/client/client.component';
+import { AccountsComponent } from '../pages/client/accounts/accounts.component';
+import { ClientComponent } from '../pages/client/client_page/client.component';
+import { DepositsComponent } from '../pages/client/deposits/deposits.component';
+import { LoansComponent } from '../pages/client/loans/loans.component';
 import { EmployeeComponent } from '../pages/employee/employee.component';
 import { EscortComponent } from '../pages/escort/escort.component';
 import { SignInComponent } from '../pages/sign-in/sign-in.component';
 import { SignUpComponent } from '../pages/sign-up/sign-up.component';
+import { AdminPanelComponent } from '../pages/admin-panel/admin-panel.component';
 
 const routes: Routes = [
   { path: '', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
+  {path: 'admin', component: AdminPanelComponent},
+  { path: 'client', component: ClientComponent, children: [
+    { path: 'accounts', component: AccountsComponent },
+    { path: 'loans', component: LoansComponent },
+    { path: 'deposits', component: DepositsComponent },
+  ]},
   { path: 'employee', component: EmployeeComponent },
-  { path: 'client', component: ClientComponent },
-  { path: 'escort', component: EscortComponent },
-  { path: 'admin', component: AdminPanelComponent }
+  { path: 'escort', component: EscortComponent }
 ];
 
 @NgModule({
